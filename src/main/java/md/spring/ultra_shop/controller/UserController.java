5fe51f8510b9ccc19287bb66f75aa6e188b09133
vadim11041996 <vbraduleac@pentalog.com>
2019-05-02 11:43:03 +0300

    add config for cors

--- src/main/java/md/spring/ultra_shop/controller/UserController.java
+++ src/main/java/md/spring/ultra_shop/controller/UserController.java
@@ -1,6 +1,7 @@
 package md.spring.ultra_shop.controller;
 
 import md.spring.ultra_shop.repository.entity.UserEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
