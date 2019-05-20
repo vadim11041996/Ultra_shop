a80f37d794b0cad5168932f436c71bfb055e534c
vadim11041996 <vbraduleac@pentalog.com>
2019-05-20 14:12:12 +0300

    Remove useles code

--- src/main/java/md/spring/ultra_shop/controller/UserController.java
+++ src/main/java/md/spring/ultra_shop/controller/UserController.java
@@ -1,10 +1,8 @@
 package md.spring.ultra_shop.controller;
 
 import md.spring.ultra_shop.repository.entity.UserEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
 
 @RequestMapping(path = "user")
